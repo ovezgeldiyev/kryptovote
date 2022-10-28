b98b0fd46e1778a795dce279586205790c4b43e7
ovezgeldiyev <ovezgeldiyevagabay2000@gmail.com>
2022-10-28 20:56:31 +0500

    sdfdsf

--- js/main.js
+++ js/main.js
@@ -17,10 +17,10 @@ const selectUser = document.getElementById("selectUser");
 const selectConfirm = document.getElementById("selectConfirm");
 const selectCancel = document.getElementById("selectCancel");
 
-const orderItemsBtn = document.getElementById("orderItemsBtn");
-const orderItems = document.getElementById("orderItems");
-const orderItemsConfirm = document.getElementById("orderItemsConfirm");
-const orderItemsCancel = document.getElementById("orderItemsCancel");
+const editItemBtn = document.getElementById("editItemBtn");
+const editItem = document.getElementById("editItem");
+const editItemConfirm = document.getElementById("editItemConfirm");
+const editItemCancel = document.getElementById("editItemCancel");
 
 let body = document.body;
 
@@ -58,8 +58,8 @@ const selectRemoveFunc = ()=> {
     selectUser.classList.remove("active");
     body.classList.remove("active");
 }
-const orderRemoveFunc = ()=> {
-  orderItems.classList.remove("active");
+const editItemRemoveFunc = ()=> {
+  editItem.classList.remove("active");
   body.classList.remove("active");
 }
 if(addBtn) {
@@ -89,13 +89,18 @@ if(selectBtn) {
     selectCancel.onclick = () => selectRemoveFunc();
 }
 
-if(orderItemsBtn) {
-  orderItemsBtn.onclick = function () {
-    orderItems.classList.toggle("active");
+if(editItemBtn) {
+  editItemBtn.onclick = function () {
+    editItem.classList.toggle("active");
       body.classList.toggle("active");
   };
-  orderItemsConfirm.onclick = () => orderRemoveFunc();
-  orderItemsCancel.onclick = () => orderRemoveFunc();
+  editItemConfirm.onclick = () => editItemRemoveFunc();
+  editItemCancel.onclick = () => editItemRemoveFunc();
+  window.addEventListener("click", function (e) {
+    if (e.target == editItem) {
+        editItemRemoveFunc();
+    };
+});
 }
 window.addEventListener("click", function (event) {
     if (event.target == addUser || event.target == importUser || event.target == selectUser) {
@@ -143,5 +148,65 @@ function onTabClick(tabBtns, tabItems, item) {
 
 
 
+function slist (target) {
+  // (A) SET CSS + GET ALL LIST ITEMS
+  target.classList.add("slist");
+  let items = target.getElementsByTagName("tr"), current = null;
+
+  // (B) MAKE ITEMS DRAGGABLE + SORTABLE
+  for (let i of items) {
+    // (B1) ATTACH DRAGGABLE
+    i.draggable = true;
+    
+    // (B2) DRAG START - YELLOW HIGHLIGHT DROPZONES
+    i.ondragstart = (ev) => {
+      current = i;
+      for (let it of items) {
+        if (it != current) { it.classList.add("hint"); }
+      }
+    };
+    
+    // (B3) DRAG ENTER - RED HIGHLIGHT DROPZONE
+    i.ondragenter = (ev) => {
+      if (i != current) { i.classList.add("active"); }
+    };
+
+    // (B4) DRAG LEAVE - REMOVE RED HIGHLIGHT
+    i.ondragleave = () => {
+      i.classList.remove("active");
+    };
+
+    // (B5) DRAG END - REMOVE ALL HIGHLIGHTS
+    i.ondragend = () => { for (let it of items) {
+        it.classList.remove("hint");
+        it.classList.remove("active");
+    }};
+ 
+    // (B6) DRAG OVER - PREVENT THE DEFAULT "DROP", SO WE CAN DO OUR OWN
+    i.ondragover = (evt) => { evt.preventDefault(); };
+ 
+    // (B7) ON DROP - DO SOMETHING
+    i.ondrop = (evt) => {
+      evt.preventDefault();
+      if (i != current) {
+        let currentpos = 0, droppedpos = 0;
+        for (let it=0; it<items.length; it++) {
+          if (current == items[it]) { currentpos = it; }
+          if (i == items[it]) { droppedpos = it; }
+        }
+        if (currentpos < droppedpos) {
+          i.parentNode.insertBefore(current, i.nextSibling);
+        } else {
+          i.parentNode.insertBefore(current, i);
+        }
+      }
+    };
+  }
+}
+
+
+window.addEventListener("DOMContentLoaded", () => {
+  slist(document.getElementById("sortlist"));
+});
 
 
